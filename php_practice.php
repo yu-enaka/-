432448bf4a41a637457c60f312e5b356a3c4d75e
”yu” <enaka@opzt.net>
2020-06-11 11:16:38 +0000

    20200611

---
+++ php_practice.php
@@ -0,0 +1,50 @@
+<?php
+echo 'hello php!';
+
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+$array_month = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+echo $array_month["August"];
+
+$hello = "Hello,";
+$name = "enaka yu";
+$world = "'s World!";
+echo $hello . $name . $world;
+
+$tech_boost = "tech";
+$tech_boost .= "_boost";
+echo $tech_boost;
+
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar_2018["December"];
